78a9cb75346d6cea8e0155ffabe1981ad337ac6f
Nata <6130970@gmail.com>
2022-03-18 17:00:32 +0300

    add PageObjectStatic

---
+++ src/test/java/PageObjectStatic/HomePageStaticTest.java
@@ -0,0 +1,33 @@
+package PageObjectStatic;
+
+import PageStatic.HomePageStatic;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+import java.util.ArrayList;
+import static org.testng.Assert.assertTrue;
+
+public class HomePageStaticTest extends TestBaseStatic {
+
+    @Test
+    public void duckClickRubberDarkTest() {
+        String title;
+        title = HomePageStatic.duckClickRubberDark(driver);
+        assertTrue(title.contains("Rubber Ducks"), "Rubber Ducks not found");
+    }
+
+    @Test
+    public void duckClickDeliveryInformationTest() {
+        String footer = "";
+
+        footer = HomePageStatic.duckClickDeliveryInformation(driver);
+        assertTrue(footer.contains("Delivery Information"),"Delivery Information not found");
+    }
+
+    @Test
+    public void duckClickTermConditionTest() {
+        ArrayList<WebElement> listElements;
+        String footer = "";
+        footer = HomePageStatic.duckClickTermCondition(driver);
+        assertTrue(footer.contains("Terms & Conditions"),"Terms & Conditions not found");
+    }
+}
